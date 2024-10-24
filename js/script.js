cc3b5cbf205409a0fdf8f4ae73c64c33e8fa6ceb
LadyFantasy <msoledadm88@gmail.com>
2024-10-24 04:16:37 +0200

    script js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Detectar el scroll y aplicar la clase 'scrolled' al navbar
+/* window.addEventListener('scroll', function() {
+  var navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) { // Si el scroll es mayor a 50px
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+}); */
+
+/* Carrousel */
