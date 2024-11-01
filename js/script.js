08e2582ee8fa677c1949555543dcdda1bf73b802
LadyFantasy <msoledadm88@gmail.com>
2024-11-01 02:47:32 +0100

    fix nav

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 window.addEventListener("scroll", function () {
   var navbar = document.querySelector("nav");
   if (window.scrollY > 111) {
-    // Si el scroll es mayor a 150px
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
@@ -15,40 +14,38 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const hero = document.querySelector(".hero");
 const points = document.querySelectorAll(".point");
-const hero_carrousel= document.querySelectorAll(".hero_carrousel_img_text_wrapper")
-const product_carrousel = document.querySelector(".slider_product")
-const product_arrow = document.querySelector(".arrows_products")
+const hero_carrousel = document.querySelectorAll(".hero_carrousel_img_text_wrapper");
+const product_carrousel = document.querySelector(".slider_product");
+const product_arrow = document.querySelector(".arrows_products");
 
 let currentIndexHero = 0;
 let currentIndexProduct = 0;
 
 const showSlide = index => {
-  if(index<3){
+  if (index < 3) {
     slides[currentIndexHero].classList.remove("active");
     slides[currentIndexHero].classList.add("deactivate");
-    hero_carrousel[currentIndexHero].classList.remove("move_up")
-    hero_carrousel[currentIndexHero].classList.add("move_down")
+    hero_carrousel[currentIndexHero].classList.remove("move_up");
+    hero_carrousel[currentIndexHero].classList.add("move_down");
     setTimeout(() => {
       slides[currentIndexHero].classList.remove("deactivate");
       slides[index].classList.add("active");
-      hero_carrousel[index].classList.remove("move_down")
-      hero_carrousel[index].classList.add("move_up")
+      hero_carrousel[index].classList.remove("move_down");
+      hero_carrousel[index].classList.add("move_up");
 
       currentIndexHero = index;
     }, 500);
-  }else{
-    if(index==3){
+  } else {
+    if (index == 3) {
       product_carrousel.classList.remove("move_right");
       product_carrousel.classList.add("move_left");
-    }else{
+    } else {
       product_carrousel.classList.remove("move_left");
       product_carrousel.classList.add("move_right");
     }
   }
   points[index].checked = true;
 };
-
-
 
 next.addEventListener("click", () => {
   const nextIndex = (currentIndexHero + 1) % slides.length;
@@ -66,12 +63,9 @@ points.forEach((point, index) => {
   });
 });
 
+product_arrow.addEventListener("click", () => {
+  currentIndexProduct = (currentIndexProduct - 1) * -1;
+  showSlide(currentIndexProduct + 3);
+});
 
-product_arrow.addEventListener("click",()=>{
-  currentIndexProduct = (currentIndexProduct-1)*-1;
-  showSlide(currentIndexProduct+3);
-})
-
-// Mostrar la primera imagen al cargar la página
 showSlide(currentIndexHero);
-// showSlide(currentIndexProduct+3);
